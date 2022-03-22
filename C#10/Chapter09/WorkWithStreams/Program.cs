3eb8bbd56e7f6a6368b11b97826a68d8344d04b9
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-03-22 21:07:00 +0300

    Compressed XML using GZIP Stream Method

--- C#10/Chapter09/WorkWithStreams/Program.cs
+++ C#10/Chapter09/WorkWithStreams/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.Xml;
+using System.IO.Compression; // BrotlilStream, GZipStream, CompressionMode
 using static System.Console;
 using static System.Environment;
 using static System.IO.Path;
 
 // WorkWithText();
 WorkWithXml();
+WorkWithCompression();
 
 static void WorkWithText()
 {
@@ -83,6 +85,52 @@ static void WorkWithText()
          }
       }
    }
+static void WorkWithCompression()
+{
+   string fileExt = "gzip";
+   // compress the XML output
+   string filePath = Combine(CurrentDirectory, $"streams.{fileExt}");
+   FileStream file = File.Create(filePath);
+   Stream compressor = new GZipStream(file, CompressionMode.Compress);
+   using (compressor)
+   {
+      using (XmlWriter xml = XmlWriter.Create(compressor))
+      {
+         xml.WriteStartDocument();
+         xml.WriteStartElement("callsigns");
+         foreach (string item in Viper.CallSigns)
+         {
+            xml.WriteElementString("callsign", item);
+         }
+         // the normal call to WriteEndElement is not necessary because when the XmlWriter disposes, it will automatically end any elements of any depth
+      }
+   }// also closes the underlying stream
+
+   // output all the contents of the compressed file
+   WriteLine("{0} contains {1:N0} bytes.",filePath, new FileInfo(filePath).Length);
+   WriteLine("The compressed contents:");
+   WriteLine(File.ReadAllText(filePath));
+
+   // read a compressed file
+   WriteLine("Reading the compressed XML file:");
+   file = File.Open(filePath, FileMode.Open);
+   Stream decompressor = new GZipStream(file, CompressionMode.Decompress);
+   using (decompressor)
+   {
+      using (XmlReader reader = XmlReader.Create(decompressor))
+      {
+         while (reader.Read()) // read the next XML node
+         {
+            // check if we are on an element node named callsign
+            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+            {
+               reader.Read(); // move to the text inside element
+               WriteLine($"{reader.Value}"); // read its value
+            }
+         }
+      }
+   }
+}
 static class Viper
 {
    // define an array of Viper pilot call signs
