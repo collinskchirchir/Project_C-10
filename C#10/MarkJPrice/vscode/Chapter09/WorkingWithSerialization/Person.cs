3681f8d1036725a7d8790ba7cff93c64ab6f6218
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-03-25 20:28:21 +0300

    Generating a Compact XML

--- C#10/MarkJPrice/vscode/Chapter09/WorkingWithSerialization/Person.cs
+++ C#10/MarkJPrice/vscode/Chapter09/WorkingWithSerialization/Person.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 namespace Packt.Shared;
 public class Person
 {
@@ -6,8 +7,11 @@ public class Person
    {
       Salary = initialSalary;
    }
+   [XmlAttribute("fname")]
    public string? FirstName {get; set;}
+   [XmlAttribute("lname")]
    public string? LastName {get; set;}
+   [XmlAttribute("dob")]
    public DateTime DateOfBirth {get; set;}
    public HashSet<Person>? Children {get; set;}
    protected decimal Salary {get; set;}
