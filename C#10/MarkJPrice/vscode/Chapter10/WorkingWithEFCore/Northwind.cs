78999268059b33051581639c261075d78f865eee
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-03-29 08:40:07 +0300

    Defining the Northwind database context class

--- C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Northwind.cs
+++ C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Northwind.cs
@@ -5,5 +5,21 @@ namespace Packt.Shared;
 // This manages the connection to the database
 public class Northwind : DbContext
 {
-   
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+      if (ProjectConstants.DatabaseProvider == "SQLite")
+      {
+         string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+         WriteLine($"Using {path} database file.");
+         optionsBuilder.UseSqlite($"Filename = {path}");
+      }
+      else
+      {
+         string connection = "Data Source =.;" +
+            "Initial Catalog = Northwind;" +
+            "Integrated Security = true;" +
+            "MultipleActiveResultSets = true;";
+         optionsBuilder.UseSqlServer(connection);
+      }
+   }
 }
