afc7c2dee2f180e96a6b9b264943d0586f549ce3
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-04-04 20:39:06 +0300

    Added Tables to the 'Northwind' Db Context Class

--- C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Northwind.cs
+++ C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Northwind.cs
@@ -5,6 +5,9 @@ namespace Packt.Shared;
 // This manages the connection to the database
 public class Northwind : DbContext
 {
+   // these properties map to tables in the database
+   public DbSet<Category>? Categories{get; set;}
+   public DbSet<Product>? Products{get; set;}
    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
    {
       if (ProjectConstants.DatabaseProvider == "SQLite")
@@ -20,6 +23,21 @@ public class Northwind : DbContext
             "Integrated Security = true;" +
             "MultipleActiveResultSets = true;";
          optionsBuilder.UseSqlServer(connection);
+      }
+   }
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      // example of using Fluent API instead of attributes to limit the length of category name to 15
+      modelBuilder.Entity<Category>
+         .Property(category => category.CategoryName)
+         .IsRequired() // NOT NULL
+         .HasMaxLength(15);
+      if (ProjectConstants.DatabaseProvider == "SQLite")
+      {
+         // added to "fix" the lack of decimal support in SQLite
+         modelBuilder.Entity<Product>
+            .Property(product => product.Cost)
+            .HasConversion<double>();
       }
    }
 }
