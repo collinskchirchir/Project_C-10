541479f949d56142478e953fb0c98a5df9830c92
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-04-15 12:51:30 +0100

    Querying EF Core models

--- C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Program.cs
+++ C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Program.cs
@@ -1,6 +1,30 @@
 ﻿using Packt.Shared;
 using static System.Console;
+using Microsoft.EntityFrameworkCore; // Include extension method
 
 
 
 WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+QueryingCategories();
+
+static void QueryingCategories()
+{
+   using (Northwind db = new())
+   {
+      WriteLine("Categories and how many products they have:");
+      
+      // a query to get all categories and their related products
+      IQueryable<Category>? categories = db.Categories?
+         .Include(c => c.Products);
+      if(categories is null)
+      {
+         WriteLine("No categories found.");
+         return;
+      }
+      // execute query and enumarate results
+      foreach(Category c in categories)
+      {
+         WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+      }
+   }
+}
