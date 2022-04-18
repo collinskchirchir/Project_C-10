e5c3be4bd59c2a289bd5c2f1eedded80b015b3c5
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-04-18 16:53:28 +0100

    Filtering included entities

--- C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Program.cs
+++ C#10/MarkJPrice/vscode/Chapter10/WorkingWithEFCore/Program.cs
@@ -5,7 +5,8 @@ using Microsoft.EntityFrameworkCore; // Include extension method
 
 
 WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
-QueryingCategories();
+// QueryingCategories();
+FilteredIncludes();
 
 static void QueryingCategories()
 {
@@ -21,10 +22,38 @@ static void QueryingCategories()
          WriteLine("No categories found.");
          return;
       }
-      // execute query and enumarate results
+      // execute query and enumerate results
       foreach(Category c in categories)
       {
          WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
       }
+   }
+}
+
+static void FilteredIncludes()
+{
+   using(Northwind db = new())
+   {
+      Write("Enter a minimum for units in stock: ");
+      string unitsInStock = ReadLine() ?? "10";
+      int stock = int.Parse(unitsInStock);
+
+      IQueryable<Category>? categories = db.Categories?
+         .Include(c => c.Products.Where(p => p.Stock >= stock));
+      if (categories is null)
+      {
+         WriteLine("No categories found.");
+         return;
+      }
+
+      foreach (Category c in categories)
+      {
+         WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+         foreach(Product p in c.Products)
+         {
+            WriteLine($"   {p.ProductName} has {p.Stock} units in stock.");
+         }
+      }
+
    }
 }
