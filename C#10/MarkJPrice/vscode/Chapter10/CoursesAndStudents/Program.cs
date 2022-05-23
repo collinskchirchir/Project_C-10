1fe9d6710d5b4dcb8cc9a7ac8d9a9c4c483b86bd
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-05-23 19:47:07 +0100

    Finalized Chapter10

--- C#10/MarkJPrice/vscode/Chapter10/CoursesAndStudents/Program.cs
+++ C#10/MarkJPrice/vscode/Chapter10/CoursesAndStudents/Program.cs
@@ -1,1 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore.Sqlite;
+﻿using Microsoft.EntityFrameworkCore; // for GenerateCreateScript()
+using CoursesAndStudents; // Academy
+
+using static System.Console;
+
+using (Academy a = new())
+{
+   bool deleted = await a.Database.EnsureDeletedAsync();
+   WriteLine($"Database deleted: {deleted}");
+
+   bool created = await a.Database.EnsureCreatedAsync();
+   WriteLine($"Database created: {created}");
+
+   WriteLine("SQL script used to created database:");
+   WriteLine(a.Database.GenerateCreateScript());
+
+   foreach (Student s in a.Students.Include(s => s.Courses))
+   {
+      WriteLine("{0} {1} attends the following {2} courses:",
+         s.FirstName, s.LastName, s.Courses.Count);
+      foreach (Course c in s.Courses)
+      {
+         WriteLine($"   {c.Title}");
+      }
+   }
+}
+
