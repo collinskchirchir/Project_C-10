ababb2545d6016d2c6ed73ecbb366ca4e2b57bfc
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-05-24 16:56:59 +0100

    Chapter11 again

--- C#10/MarkJPrice/vscode/Chapter11/LinqWithObjects/Program.cs
+++ C#10/MarkJPrice/vscode/Chapter11/LinqWithObjects/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// a string array is a sequence that implements IEnumerable<string>
+string[] names = new[] {"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+
+WriteLine("Deferred execution");
+
+// Question: Which names end with an M?
+// (Written using a LINQ extension method)
+var query1 = names.Where(name => name.EndsWith("m"));
+
+// Question: Which names end with
