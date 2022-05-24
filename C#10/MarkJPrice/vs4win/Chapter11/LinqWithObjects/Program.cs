fc718c1382434ec1a7e6391e97d647846e6d4922
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-05-24 20:43:28 +0100

    Chapter11 project on vs4

---
+++ C#10/MarkJPrice/vs4win/Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+// a string array is a sequence that implements IEnumerable<string>
+string[] names = new[] {"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+
+WriteLine("Deferred execution");
+
+// Question: Which names end with an M?
+// (Written using a LINQ extension method)
+var query1 = names.Where(name => name.EndsWith("m"));
+
+// Question: Which names end with an M?
+// (Written using LINQ query comprehension syntax)
+var query2 = from name in names where name.EndsWith("m") select name;
+
+// Answer returned as an array of strings containing Pam and Jim
+string[] result1 = query1.ToArray();
+
+// Answer returned as a list of strings containing Pam and Jim
+List<string> result2 = query2.ToList();
+
+// Answer returned as we enumerate overt the results
+foreach (string name in query1)
+{
+   WriteLine(name); // outputs Pam
+   names[2] = "Jimmy"; // change Jim to Jimmy
+   // on the second iteration, Jimmy does not end with an M
+}
