1a274e9941b313d6d0077adc3c6aea0ca7d13d95
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2022-06-20 18:14:49 +0100

    Projecting Sequences into new types

--- C#10/MarkJPrice/vs4win/Chapter11-vs4/LinqWithEFCore/Program.cs
+++ C#10/MarkJPrice/vs4win/Chapter11-vs4/LinqWithEFCore/Program.cs
@@ -15,8 +15,16 @@ static void FilterAndSort()
         IOrderedQueryable<Product> sortedAndFilteredProducts = 
             filteredProducts.OrderByDescending(product => product.UnitPrice);
 
+        var projectedProducts = sortedAndFilteredProducts
+            .Select(product => new // anonymous type
+            {
+                product.ProductId,
+                product.ProductName,
+                product.UnitPrice
+            });
+
         WriteLine("Products that cost less than $10:");
-        foreach (Product p in sortedAndFilteredProducts)
+        foreach (var p in projectedProducts)
         {
             WriteLine("{0}: {1} costs {2:$#,##0.00}", 
                 p.ProductId, p.ProductName, p.UnitPrice);
