485b853a9b1ac6c1f69c3bc8d60eaef41341fe3d
Collins Chirchir <collinskchirchir@gmail.com>
2022-08-24 18:19:38 +0100

    Running multiple actions synchronously

---
+++ C#10/MarkJPrice/vs4win/Chapter12/WorkingWithTasks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics; //Stopwatch
+
+using static System.Console;
+
+OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+WriteLine("Running methods synchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+
+static void OutputThreadInfo()
+{
+    Thread t = Thread.CurrentThread;
+    WriteLine(
+        "Thread Id: {0}, Priority: {1}, Background: {2}, Name: {3}",
+        t.ManagedThreadId, t.Priority, t.IsBackground, t.Name ?? "null");
+}
+static void MethodA()
+{
+    WriteLine("Starting Method A...");
+    OutputThreadInfo();
+    Thread.Sleep(3000); // simulate three seconds of work 
+    WriteLine("Finished Method A.");
+}
+
+static void MethodB()
+{
+    WriteLine("Starting Method B...");
+    OutputThreadInfo();
+    Thread.Sleep(2000); // simulate two seconds of work 
+    WriteLine("Finished Method B.");
+}
+
+static void MethodC()
+{
+    WriteLine("Starting Method C...");
+    OutputThreadInfo();
+    Thread.Sleep(1000); // simulate one second of work 
+    WriteLine("Finished Method C.");
+}
