4164b47be756c53fd6085086af8223d11c7d0949
Collins Chirchir <collinskchirchir@gmail.com>
2022-08-25 10:39:41 +0300

    Waiting for Task

--- C#10/MarkJPrice/vs4win/Chapter12/WorkingWithTasks/Program.cs
+++ C#10/MarkJPrice/vs4win/Chapter12/WorkingWithTasks/Program.cs
@@ -16,7 +16,8 @@ Task taskA = new(MethodA);
 taskA.Start();
 Task taskB = Task.Factory.StartNew(MethodB);
 Task taskC = Task.Run(MethodC);
-
+Task[] tasks = { taskA, taskB, taskC };
+Task.WaitAll(tasks);
 
 WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 
