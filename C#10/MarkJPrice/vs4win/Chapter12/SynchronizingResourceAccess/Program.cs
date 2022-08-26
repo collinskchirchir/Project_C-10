356c90593886614c715c4f639b17810df8d19238
Collins Chirchir <collinskchirchir@gmail.com>
2022-08-26 16:01:37 +0300

    Accessing a resource from multiple threads

---
+++ C#10/MarkJPrice/vs4win/Chapter12/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics; // Stopwatch
+using static System.Console;
+
+WriteLine("Please wait for the tasks to complete.");
+Stopwatch watch = Stopwatch.StartNew();
+Task a = Task.Factory.StartNew(MethodA);
+Task b = Task.Factory.StartNew(MethodB);
+
+Task.WaitAll(new Task[] { a, b });
+WriteLine();
+WriteLine($"Results: {SharedObjects.Message}.");
+WriteLine($"{watch.ElapsedMilliseconds:N0} elapsed milliseconds.");
+
+static void MethodA()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Thread.Sleep(SharedObjects.Random.Next(2000));
+        SharedObjects.Message += "A";
+        Write(".");
+    }
+}
+static void MethodB()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Thread.Sleep(SharedObjects.Random.Next(2000));
+        SharedObjects.Message += "B";
+        Write(".");
+    }
+}
+    static class SharedObjects
+{
+    public static Random Random = new();
+    public static string? Message; // a shared resource
+}
